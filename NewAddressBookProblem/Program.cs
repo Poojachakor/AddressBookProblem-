4c321ba28041e7d6194b24fc64ad1068dfc64102
Poojachakor <chakorpooja07@gmail.com>
2023-06-30 20:13:49 +0530

    Add: UC11 file

--- NewAddressBookProblem/Program.cs
+++ NewAddressBookProblem/Program.cs
@@ -24,7 +24,7 @@
             do
             {
                 Console.WriteLine($"Working On {bookName} AddressBook\n");
-                Console.WriteLine("Choose An Option \n1.Add New Contact \n2.Edit Existing Contact \n3.Delete A Contact \n4.View A Contact \n5.View All Contacts \n6.Add New AddressBook \n7.Switch AddressBook \n8.Search Contact by City/State \n9.Count by City/State \n0.Exit Application\n");
+                Console.WriteLine("Choose An Option \n1.Add New Contact \n2.Edit Existing Contact \n3.Delete A Contact \n4.View A Contact \n5.View All Contacts \n6.Add New AddressBook \n7.Switch AddressBook \n8.Search Contact by City/State \n9.Count by City/State \n10.Sort Entries By Name \n0.Exit Application\n");
                 choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -121,6 +121,9 @@
                         break;
                     case 9:
                         addressBook.DisplayCountByCityandState();
+                        break;
+                    case 10:
+                        addressBook.SortByName();
                         break;
                     case 0:
                         Console.WriteLine("Thank You For Using Address Book System.");
