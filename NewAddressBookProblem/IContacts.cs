e45bbcd0e50342f345a46a8201b32be3f9f6ffba
Poojachakor <chakorpooja07@gmail.com>
2023-06-30 18:10:49 +0530

    Add: UC4 file

--- NewAddressBookProblem/IContacts.cs
+++ NewAddressBookProblem/IContacts.cs
@@ -9,6 +9,8 @@ namespace NewAddressBookProblem
     interface IContacts
     {
         public void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber);
+        public void ViewContact(string name);
         public void EditContact(string name);
+        public void DeleteContact(string name);
     }
 }
