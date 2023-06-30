99e7f5c16487918229c53c65f1dc710525fc0cf1
Poojachakor <chakorpooja07@gmail.com>
2023-06-30 17:53:37 +0530

    Add: NewAddressBookProblem

---
+++ NewAddressBookProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace NewAddressBookProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContact("Pooja", "Date", "Panchvati", "Nashik", "MH", "datepooja07@gmail.com", 422003,7040613418);
+            addressBook.ViewContact();
+        }
+    }
+}
